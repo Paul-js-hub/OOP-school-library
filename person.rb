13aafae04a0ad3570384931a075da25a6e835a81
Paul Onchera <hesbonpoisson@gmail.com>
2022-08-24 16:26:01 +0300

    Implement classes to represent students and teachers

---
+++ person.rb
@@ -0,0 +1,25 @@
+class Person
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def initialize(name, age = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  private
+
+  def of_age?
+    age >= 18
+  end
+
+  def can_use_services?
+    if age >= 18 || parent_permission
+      true
+    else
+      false
+    end
+  end
+end
---
+++ teacher.rb
@@ -0,0 +1,11 @@
+require_relative 'person'
+class Teacher < Person
+  def initialize(specialization)
+    super()
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
